44ad2f1dec8857260eed66f8dcf1f601e4a1b207
Sam Meyer <sammie_8288@hotmail.com>
2016-03-11 17:08:55 +0100

    Basic functionality test

--- FunctionalGame/GameMain.fs
+++ FunctionalGame/GameMain.fs
@@ -3,40 +3,63 @@
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
 open Microsoft.Xna.Framework.Content
+open Microsoft.Xna.Framework.Input
+open PlatformerActor
+open PlatformerAnimation
+open PlatformerInput
+open PlatformerPhysics
 
 type GameMain () as x =
     inherit Game()
- 
-    do x.Content.RootDirectory <- "Content"
+
+    do x.Content.RootDirectory <- ""
+    #if INTERACTIVE
+    do x.Content.RootDirectory <- __SOURCE_DIRECTORY__ + @"bin\Debug\Content"
+    #endif
     let graphics = new GraphicsDeviceManager(x)
     let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
+
+    let CreateActor' = CreateActor x.Content
+
+    let mutable WorldObjects = lazy ([("animtest.png", Player(Nothing), Vector2(10.f,28.f), Vector2(32.f,32.f), false);
+                                      ("obstacle.png", Obstacle, Vector2(10.f,60.f), Vector2(32.f,32.f), true);
+                                      ("", Obstacle, Vector2(42.f,60.f), Vector2(32.f,32.f), true);]
+                                     |> List.map CreateActor')
+
+    let DrawActor (sb:SpriteBatch) actor =
+        if actor.Animation.IsSome then 
+            do DrawAnimation sb actor.Animation.Value actor.Position
+        ()
 
     override x.Initialize() =
         do spriteBatch <- new SpriteBatch(x.GraphicsDevice)
         do base.Initialize()
-         
-         // TODO: Add your initialization logic here
-
         ()
 
     override x.LoadContent() =
-        
-         // TODO: use this.Content to load your game content here   
-        
+        do WorldObjects.Force () |> ignore
         ()
- 
-    override x.Update (gameTime) =
 
-         // TODO: Add your update logic here
-        
+    override x.Update (gameTime) =
+        let AddGravity' = AddGravity gameTime
+        let HandleInput' = HandleInput (Keyboard.GetState ())
+        let UpdateActorAnimation' = UpdateActorAnimation gameTime
+        let current = WorldObjects.Value
+        do WorldObjects <- lazy (current
+                                 |> List.map HandleInput'
+                                 |> List.map AddGravity'
+                                 |> List.map AddFriction
+                                 |> HandleCollisions
+                                 |> List.map ResolveVelocities
+                                 |> List.map UpdateActorAnimation')
+        do WorldObjects.Force () |> ignore
         ()
- 
+
     override x.Draw (gameTime) =
         do x.GraphicsDevice.Clear Color.CornflowerBlue
-        
-        // TODO: Add your drawing code here
-
+        let DrawActor' = DrawActor spriteBatch
+        do spriteBatch.Begin ()
+        WorldObjects.Value
+        |> List.iter DrawActor'
+        do spriteBatch.End ()
         ()
-
-
-
---
+++ FunctionalGame/PlatformerActor.fs
@@ -0,0 +1,54 @@
+﻿module PlatformerActor
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Content
+open PlatformerAnimation
+
+type BodyType =
+    | Static
+    | Dynamic of Vector2
+
+type PlayerState =
+    | Nothing
+    | Jumping
+
+type ActorType =
+    | Player of PlayerState
+    | Obstacle
+
+type WorldActor =
+    {
+        ActorType : ActorType;
+        Position : Vector2;
+        Size : Vector2;
+        Animation : Animation option;
+        BodyType : BodyType
+    }
+    member this.CurrentBounds
+        with get () = Rectangle((int this.Position.X),(int this.Position.Y),(int this.Size.X),(int this.Size.Y))
+
+    member this.DesiredBounds
+        with get () = let desiredPos = match this.BodyType with
+                                       | Dynamic(s) -> this.Position + s
+                                       | _-> this.Position
+                      Rectangle((int desiredPos.X), (int desiredPos.Y), (int this.Size.X), (int this.Size.Y))
+
+let CreateActor (content:ContentManager) (textureName, actorType, position, size, isStatic) =
+    let tex = if not (System.String.IsNullOrEmpty textureName) then
+                  let tex = content.Load<Texture2D>(textureName)
+                  let anim = CreateAnimation tex 100
+                  Some(anim)
+              else
+                  None
+    let bt = if isStatic then
+                Static
+             else
+                Dynamic(Vector2(0.f,0.f))
+    { ActorType = actorType; Position = position; Size = size; Animation = tex; BodyType = bt; }
+
+let UpdateActorAnimation gameTime (actor:WorldActor) =
+    let animation = if actor.Animation.IsSome then
+                        Some(UpdateAnimation gameTime actor.Animation.Value)
+                    else None
+    { actor with Animation = animation }
